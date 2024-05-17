9dd9cf48f711b1d72d8f0292b57512d68a8d5ef8
Kelvin Tsang <ctsang94@gmail.com>
2024-05-17 00:05:19 -0400

    Reduce partials and move hero, section, gallery, and comment into bio.scss

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,26 +1,24 @@
 const form = document.getElementById('commentForm');
 const comments = [
     {
-        name: 'Ruth',
+        name: 'Tiesto',
         comment: 'I dont get it',
         date: '5/12/2024',
-        date2: '5/12/2024'
+        date2: new Date()
     },
     {
         name: 'John',
         comment: 'I love Javascript!!!!',
         date: '5/02/2024',
-        date2: '5/02/2024'
+        date2: new Date()
     },
     {
         name: 'Tim',
         comment: 'Not me, I hate JAVASCRIPT!!!!!',
         date: '5/03/2024',
-        date2: '5/03/2024'
+        date2: new Date()
     }
 ];
-
-displayComments(comments);
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -31,14 +29,14 @@ form.addEventListener('submit', (e) => {
         name: name, comment: comment, date: date, date2: new Date()
     };
     comments.push(newComment);
+    comments.sort((a, b) => b.date2 - a.date2);
+    console.log(comments);
     displayComments(comments);
 });
 
 function displayComments(comments) {
     const commentsContainer = document.querySelector('.comment-section');
     commentsContainer.innerHTML = '';
-
-    comments.sort((a, b) => b.date2 - a.date2);
     comments.forEach(comment => {
         const commentElement = document.createElement('span');
         const commentElement2 = document.createElement('span');
