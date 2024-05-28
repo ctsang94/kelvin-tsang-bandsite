6cccdbf0fc982f65ede61846ba6eb31ff88c5b4e
Kelvin Tsang <ctsang94@gmail.com>
2024-05-28 00:00:24 -0400

    Removed comments

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,34 +1,5 @@
 const form = document.querySelector(".commentform");
-// const comments = [
-// 	{
-// 		name: "Tiesto",
-// 		comment:
-// 			"This is art. This is inexplicable magic expressed in the purest way,ever thing that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
-// 		date: "5/02/2024",
-// 		date2: new Date(),
-// 	},
-// 	{
-// 		name: "John",
-// 		comment:
-// 			"This is art. This is inexplicable magic expressed in the purest way,everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
-// 		date: "5/02/2024",
-// 		date2: new Date(),
-// 	},
-// 	{
-// 		name: "Tim",
-// 		comment:
-// 			"This is art. This is inexplicable magic expressed in the purest way,everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
-// 		date: "5/03/2024",
-// 		date2: new Date(),
-// 	},
-// 	{
-// 		name: "Tim",
-// 		comment:
-// 			"This is art. This is inexplicable magic expressed in the purest way,everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
-// 		date: "5/03/2024",
-// 		date2: new Date(),
-// 	},
-// ];
+
 let comments = [];
 
 async function fetchAndStoreComments() {
